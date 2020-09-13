5505c3345926b1b1d4eb5bb4c7e0ffea01c2382b
Allison Ricker <alliricker@gmail.com>
2020-09-13 14:04:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,6 +39,7 @@ def turn(board)
   index = input_to_index(input)
    if valid_move?(board,index)
      move(board, index, character = "X")
-     turn(board)
+     else
+       turn(board)
    end
 end
